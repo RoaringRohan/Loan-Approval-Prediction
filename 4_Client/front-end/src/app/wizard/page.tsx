49767060cc310b8baecf7e78bc26253bfbfd9b2e
Rohan Datta <90877887+RoaringRohan@users.noreply.github.com>
2025-04-03 02:01:58 -0400

    Updating fetch url to be from Render Web Service

--- 4_Client/front-end/src/app/wizard/page.tsx
+++ 4_Client/front-end/src/app/wizard/page.tsx
@@ -206,7 +206,7 @@ export default function Wizard() {
 
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:8080/predict", {
+      const response = await fetch("https://cs4442b-project.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
